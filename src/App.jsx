f0107a5caadd14a70e38bf84e59a96a1e0946b7b
C:\Users\HP <hyllusx@gmail.com>
2026-01-27 09:55:54 +0200

    Created Dashboard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
       </Routes>
     </>
