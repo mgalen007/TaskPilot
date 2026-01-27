d6c7096fe590167c20704004e0c450f305ae5ee9
C:\Users\HP <hyllusx@gmail.com>
2026-01-27 08:51:16 +0200

    Enhance button effects

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import Icon from '../components/Icon'
 function LoginPage() {
     return (
         <>
-            <div className="border-[.5px] border-gray w-fit mx-auto mt-[20vh] pt-6 pb-12 px-9 rounded-2xl">
+            <div className="border-[.5px] border-gray w-fit mx-auto mt-[20vh] pt-5 pb-12 px-9 rounded-2xl">
                 <Icon width={300} height={100} />
                 <form className="mt-2">
                     <div>
@@ -31,7 +31,7 @@ function LoginPage() {
                             <input 
                                 type="submit" 
                                 value="Continue →"
-                                className="bg-[#fae150] w-[100%] mt-[4vh] py-2 px-3 rounded-2xl cursor-pointer hover:bg-[#fae150]/80"
+                                className="bg-[#fae150] w-[100%] mt-[4vh] py-2 px-3 rounded-2xl cursor-pointer hover:bg-[#fae150]/80 active:bg-[#fae150]"
                             ></input>
                         </div>
                     </div>
