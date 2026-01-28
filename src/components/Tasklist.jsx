195d0956c3fe06cb9b2f498c51ed2d7593bd0ae8
C:\Users\HP <hyllusx@gmail.com>
2026-01-28 21:49:43 +0200

    Enhanced Task component styling

--- src/components/Tasklist.jsx
+++ src/components/Tasklist.jsx
@@ -20,8 +20,12 @@ function Task({ task }) {
     return (
         <div className="p-4 pr-5 border-t-1 border-gray-300 flex items-center justify-between">
             <div className="flex items-center gap-3">
-                <input type="checkbox" onClick={handleCheckClick} className="w-4 h-4"></input>
-                <p className={ done ? "translate-y-[-1px] line-through" :  "translate-y-[-1px]"}>{task.name}</p>
+                <input 
+                    type="checkbox" 
+                    onClick={handleCheckClick} 
+                    className="w-5 h-5 appearance-none border-1 border-gray-500 rounded-[50%] checked:bg-[#fae150] checked:border-black"
+                ></input>
+                <p className={ done ? "translate-y-[-1px] line-through text-gray-500" :  "translate-y-[-1px]"}>{task.name}</p>
             </div>
             <div>
                 <p>{task.dueDate}</p>
